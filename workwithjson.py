e102f39a7d26be53fab7074ae4ae817b44e2d9dc
VictoriaVash <vash-200@mail.ru>
2022-07-17 17:01:18 +0500

    create some method

--- workwithjson.py
+++ workwithjson.py
@@ -108,15 +108,44 @@ class Module:
     def __init__(self, productname):
         self.productname = productname
         self.access = 0
-        self.lessonslist = []
-        self.modules = {}
+        self.lessons_list = []
+        self.users_get_module = []
+        self.users_give_homework = []
+        self.finish_users = []
+        self.module = {}
+
 
     def modulelessons(self):
         pass
 
     def create_module(self, module_name):
-        self.modules['module_name'] = module_name
+        self.module['module_name'] = module_name
 
+    def add_lessons(self, *lessons_name):
+        for lesson in lessons_name:
+            self.lessons_list.append(lesson)
+
+        self.module['lessons'] = self.lessons_list
+
+    def del_lessons(self, *lessons_name):
+        for lesson in lessons_name:
+            self.lessons_list.remove(lesson)
+
+    def users_can_get_module(self, *users_id):
+        for user in users_id:
+            self.users_get_module.append(user)
+
+        self.module['users_can_get_module'] = self.users_get_module
+
+    def users_gived_homework(self, *users_id):
+        for user in users_id:
+            self.users_give_homework.append(user)
+        self.module['users_gived_homework'] = self.users_give_homework
+
+    def finished_users(self, *users_id):
+        for user in users_id:
+            self.finish_users.append(user)
+        self.module['finished_users'] = self.finish_users
 
     def editmodule(self):
         pass
@@ -125,15 +154,47 @@ class Module:
         pass
     def print_info(self):
         print(self.productname)
-        print(self.modules)
+        print(self.module)
+
 
 
 class Lesson:
-    def __init__(self, lessonname):
-        self.lessonname = lessonname
+    def __init__(self, lesson_name):
+        self.lesson_name = lesson_name
         self.access = 0
         self.boolean = 0
+        # self.content_dict = {'text': [], 'media':[]}
+        self.lesson = {}
+        self.create_lesson(self.lesson_name)
+        self.users_get_lesson = []
+        self.users_give_homework = []
+        self.finish_users = []
 
+    def create_lesson(self, lesson_name):
+        self.lesson['lesson_name'] = lesson_name
+
+    # def add_content(self, *text_content, *media_content):
+    #     for text in text_content:
+    #         self.content_dict['text'] = text
+    #
+    #     for media in media_content:
+    #         self.content_dict['media'] = media
+
+    def users_can_get_lesson(self, *users_id):
+        for user in users_id:
+            self.users_get_lesson.append(user)
+
+        self.lesson['users_can_get_lesson'] = self.users_get_lesson
+
+    def users_gived_homework(self, *users_id):
+        for user in users_id:
+            self.users_give_homework.append(user)
+        self.lesson['users_gived_homework'] = self.users_give_homework
+
+    def finished_users(self, *users_id):
+        for user in users_id:
+            self.finish_users.append(user)
+        self.lesson['finished_users'] = self.finish_users
     def addvideolesson(self):
         pass
 
@@ -148,6 +209,8 @@ class Lesson:
 
     def editlesson(self):
         pass
+    def print_info(self):
+        print(self.lesson)
 
 
 class Homework:
@@ -183,6 +246,17 @@ erik.print_moder()
 
 svyatoslav.add_moderator_list(erik.return_moder_info(), vika.return_moder_info())
 gide = Module('Revit')
+gide.add_lessons('history','english')
 gide.print_info()
+gide.del_lessons('history')
+gide.users_can_get_module('vika', 'erik')
+gide.users_gived_homework('erik')
+gide.finished_users('erik')
+gide.print_info()
+lesson = Lesson('Revit')
+lesson.users_can_get_lesson('vanya','any')
+lesson.users_gived_homework('any')
+lesson.finished_users('any')
+lesson.print_info()
 
 # uzver = User('uzver')
